5330a30299e30f43265a926984dae6368f3bb917
Serhii Babich <serhii.babich@datarobot.com>
2025-05-14 08:09:32 +0300

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import "./utils/i18n";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./styles/index.css";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
